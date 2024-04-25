7598646630d22ec939b7c5f747f31951fad4ea67
ivansanchez90 <sanchezoliva.ivan@gmail.com>
2024-04-25 18:00:36 -0300

    agregando form y validaciones para el imput

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,3 +1,40 @@
+import { useState, ChangeEvent, useMemo } from 'react'
+
 export default function BudgetForm() {
-  return <div>BudgetForm</div>
+  const [budget, setBudget] = useState(0)
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setBudget(+e.target.value)
+  }
+
+  const isValid = useMemo(() => {
+    return isNaN(budget) || budget <= 0
+  }, [budget])
+  return (
+    <form className='space-y-5 '>
+      <div className='flex flex-col space-y-5'>
+        <label
+          htmlFor='budget'
+          className='text-4xl text-blue-600 font-bold text-center'
+        >
+          Definir Presupuesto
+        </label>
+        <input
+          type='number'
+          className='w-full bg-white border border-gray-200 p-2'
+          placeholder='Define tu presupuesto'
+          name='budget'
+          id='budget'
+          value={budget}
+          onChange={handleChange}
+        />
+      </div>
+      <input
+        type='submit'
+        value='Definir Presupuesto'
+        className='bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-40'
+        disabled={isValid}
+      />
+    </form>
+  )
 }
